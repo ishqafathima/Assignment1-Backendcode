c443d5335715f2e9ef702c0d7d7cdb758b90572a
ishqafathima <149235722+ishqafathima@users.noreply.github.com>
2026-08-23 20:24:53 +0530

    Add files via upload

---
+++ src/main/java/vau/ac/lk/Assignment01/model/Product.java
@@ -0,0 +1,43 @@
+package vau.ac.lk.Assignment01.model;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int quantity;
+
+    public int getId()
+    {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
+
